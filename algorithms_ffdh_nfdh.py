aacda72cd40134692e005debb1608a7f936ab595
Silvan Suter <sili.suter@bluewin.ch>
2023-09-12 11:29:35 +0200

    improved commenting

--- algorithms_ffdh_nfdh.py
+++ algorithms_ffdh_nfdh.py
@@ -1,85 +1,143 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 10 15:18:40 2023
-
-@author: sisuter
-"""
-
 def order_by_height(rectangles):
+    """
+    Sorts the given list of rectangles in descending order by height in-place.
+
+    Args:
+    - rectangles (list of tuple): List of rectangles where each rectangle is a tuple (width, height).
+    
+    Returns:
+    - None: Modifies the input list in-place.
+    """
     rectangles.sort(reverse=True)
 
 
 def nfdh(rectangles,stripwidth=1,return_map=False):
+    """
+    Packs the rectangles using the Next Fit Decreasing Height (NFDH) algorithm.
+
+    Args:
+    - rectangles (list of tuple): List of rectangles where each rectangle is a tuple (width, height).
+    - stripwidth (int, optional): Maximum width of the strip. Defaults to 1.
+    - return_map (bool, optional): Whether to return a map indicating the position of each rectangle. Defaults to False.
+
+    Returns:
+    - tuple: Packed shelves of rectangles, heights of each shelf, and optionally a map indicating position of each rectangle.
+    """
+    # Sort rectangles by height in descending order
     rectangles_sorted = sorted(rectangles,key = lambda i:i[1],reverse=True)
-    if return_map == True:
+    if return_map:
+        # Create a map for the order of rectangles by height
         rectangles_height_order = sorted(range(len(rectangles)),key = lambda i:rectangles[i][1],reverse=True)
         rectangles_height_map = sorted(range(len(rectangles)),key = lambda i: rectangles_height_order[i])
         
-    #do nf
+    # Initialize shelves and other data structures
     S = [[rectangles_sorted[0]]]
     w_S = [rectangles_sorted[0][0]]
     h_S = [rectangles_sorted[0][1]]
     rectangles_map_for_sorted = [(0,0)]
     for i in range(1,len(rectangles_sorted)):
+        # If the rectangle fits in the current shelf, add it
         if w_S[-1] + rectangles_sorted[i][0] <= stripwidth:
             S[-1].append(rectangles_sorted[i])
             w_S[-1] += rectangles_sorted[i][0]
+        # Otherwise, start a new shelf
         else:
             S.append([rectangles_sorted[i]])
             w_S.append(rectangles_sorted[i][0])
             h_S.append(rectangles_sorted[i][1])
-        if return_map == True:
+        # Update rectangle map for sorted list
+        if return_map:
             rectangles_map_for_sorted.append((len(S)-1,len(S[-1])-1))
-    if return_map == True:
+    if return_map:
         rectangles_map = [rectangles_map_for_sorted[rectangles_height_map[i]] for i in range(len(rectangles))]
         return S, h_S, rectangles_map
     return S, h_S
 
 def ffdh(rectangles,stripwidth=1,return_map=False):
+    """
+    Packs the rectangles using the First Fit Decreasing Height (FFDH) algorithm.
+
+    Args:
+    - rectangles (list of tuple): List of rectangles where each rectangle is a tuple (width, height).
+    - stripwidth (int, optional): Maximum width of the strip. Defaults to 1.
+    - return_map (bool, optional): Whether to return a map indicating the position of each rectangle. Defaults to False.
+
+    Returns:
+    - tuple: Packed shelves of rectangles, heights of each shelf, and optionally a map indicating position of each rectangle.
+    """
+    # Sort rectangles by height in descending order
     rectangles_sorted = sorted(rectangles,key = lambda i:i[1],reverse=True)
-    if return_map == True:
+    if return_map:
+        # Create a map for the order of rectangles by height
         rectangles_height_order = sorted(range(len(rectangles)),key = lambda i:rectangles[i][1],reverse=True)
         rectangles_height_map = sorted(range(len(rectangles)),key = lambda i: rectangles_height_order[i])
         
-    #do ff
+    # Initialize shelves and other data structures
     S = [[rectangles_sorted[0]]]
     w_S = [rectangles_sorted[0][0]]
     h_S = [rectangles_sorted[0][1]]
     rectangles_map_for_sorted = [(0,0)]
     for i in range(1,len(rectangles_sorted)):
         fit_in = False
+        # Iterate over all opened shelves
         for j in range(len(S)):
+            # If the rectangle fits in the currently considered shelf, add it
             if w_S[j] + rectangles_sorted[i][0] <= stripwidth:
                 S[j].append(rectangles_sorted[i])
                 w_S[j] += rectangles_sorted[i][0]
                 fit_in = True
-                if fit_in == True and return_map == True:
+                # Update rectangle map for sorted list
+                if fit_in and return_map:
                     rectangles_map_for_sorted.append((j,len(S[j])-1))
                 break
-        if fit_in == True:
+        if fit_in:
             continue
+        # If the rectangle fits into none of the previously opened shelves, start a new one
         else:
             S.append([rectangles_sorted[i]])
             w_S.append(rectangles_sorted[i][0])
             h_S.append(rectangles_sorted[i][1])
-            if return_map == True:
+            # Update rectangle map for sorted list
+            if return_map:
                 rectangles_map_for_sorted.append((len(S)-1,0))
-                
-    if return_map == True:
+    
+    if return_map:
         rectangles_map = [rectangles_map_for_sorted[rectangles_height_map[i]] for i in range(len(rectangles))]
         return S, h_S, rectangles_map
     return S, h_S
 
 def calc_coords_of_shelf_packing(shelves,shelf_heights):
+    """
+    Calculates the coordinates for each rectangle given their shelves and shelf heights.
+
+    Args:
+    - shelves (list of list): Packed shelves of rectangles.
+    - shelf_heights (list of int): Heights of each shelf.
+
+    Returns:
+    - list: List of rectangles with their placement coordinates.
+    """
+    # Initialize an empty list to store the placed rectangles with their coordinates
     rectangles_placed = []
+    
+    # Start from the base (H=0) for the first shelf
     H = 0
+    
+    # Loop through each shelf
     for i in range(len(shelves)):
+        # Reset the width (W=0) for every new shelf
         W = 0
+        # Loop through each rectangle in the current shelf
+        
         for j in range(len(shelves[i])):
+            # Append the rectangle with its placement coordinates (bottom-left corner)
             rectangles_placed += [((W,H),shelves[i][j][0],shelves[i][j][1])]
+            # Increment the width to fit the left side of the next rectangle in the same shelf
             W += shelves[i][j][0]
+            
+        # Increment the height to fit the bottom of the next shelf
         H += shelf_heights[i]
+        
     return rectangles_placed
 
 
--- polygon_packing.py
+++ polygon_packing.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 10 16:18:30 2023
-
-@author: sisuter
-"""
-
-
 import numpy as np
 from algorithms_ffdh_nfdh import *
 from rectangle_bin_plotter import *
@@ -15,91 +6,165 @@ import numpy as np
 from fractions import Fraction
 
 def compute_polygon_width(polygon, return_vertices=False):
+    """Compute the width of a polygon based on its vertices.
+    
+    Args:
+    - polygon (list of tuple): A list of vertices of the polygon.
+    - return_vertices (bool, optional): If set to True, the function also returns the leftmost and rightmost vertices of the polygon. Defaults to False.
+
+    Returns:
+    - float or tuple: Width of the polygon. If return_vertices is True, it returns the width, leftmost vertex, and rightmost vertex.
+    """
+    # Find the leftmost and rightmost vertices
     min_vert = min(polygon,key=lambda i:i[0])
     max_vert = max(polygon,key = lambda i:i[0])
     
-    if return_vertices == True:
+    # If return_vertices flag is set, return width along with leftmost and rightmost vertices
+    if return_vertices:
         return max_vert[0] - min_vert[0], min_vert, max_vert
+    # Otherwise, just return width of the polygon
     else:
         return max_vert[0] - min_vert[0]
 
 def compute_polygon_height(polygon, return_vertices=False):
+    """Compute the height of a polygon based on its vertices.
+
+    Args:
+    - polygon (list of tuple): A list of vertices of the polygon.
+    - return_vertices (bool, optional): If set to True, the function also returns the bottommost and topmost vertices of the polygon. Defaults to False.
+
+    Returns:
+    - float or tuple: Height of the polygon. If return_vertices is True, it returns the height, bottommost vertex, and topmost vertex.
+    """
+    # Find the bottommost and topmost vertices
     min_vert = min(polygon,key=lambda i:i[1])
     max_vert = max(polygon,key = lambda i:i[1])
-        
+    
+    # If return_vertices flag is set, return height along with leftmost and rightmost vertices
     if return_vertices == True:
         return max_vert[1] - min_vert[1], min_vert, max_vert
+    # Otherwise, just return height of the polygon
     else:
         return max_vert[1] - min_vert[1]
     
 def compute_max_min_vertices_wrt_vector(polygon,vector):
+    """
+    Determines the vertices of the polygon with maximum and minimum dot products with respect to a given vector.
+    These vertices correspond to the points that are furthest / least furthest in the diretion of the vector.
+
+    Args:
+    - polygon (list of tuple): A list of vertices of the polygon.
+    - vector (tuple): A given vector.
+
+    Returns:
+    - tuple: Vertices with the minimum and maximum dot product values.
+    """
     min_vert = min(polygon,key=lambda i: np.dot(vector,i))
     max_vert = max(polygon,key=lambda i: np.dot(vector,i))
     
     return min_vert, max_vert
 
 def compute_bounding_parallelogram(polygon):
-    #needs polygons to be leftadjusted and bottomadjusted!
+    """
+    Compute the bounding parallelogram of a given polygon.
+
+    Args:
+    - polygon (list of tuple): A list of vertices of the left- and bottom-adjusted polygon. Left- and bottom-adjusted means that the lowest x-coordinate vertex of the polygon has x-coordinate 0 and similar for the lowest y-coordinate vertex.
+
+    Returns:
+    - tuple: The bounding parallelogram and the location of its left lower corner relative to the coordinate system of the polygon.
+    """
     height, min_h_vert, max_h_vert = compute_polygon_height(polygon, return_vertices=True)
+    # The spine connects a topmost and bottommost vertice
     spine = ([min_h_vert,max_h_vert])
     wside = spine[1][0] - spine[0][0]
+    # Compute normal (perpendicular) to the spine
     normal_to_spine = ([max_h_vert[1]-min_h_vert[1],min_h_vert[0]-max_h_vert[0]])
     
+    # Find the leftmost and rightmost vertices with respect to the spine
     leftest_wrt_spine, rightest_wrt_spine = compute_max_min_vertices_wrt_vector(polygon,normal_to_spine)
     right_lower_point = (rightest_wrt_spine[0] - wside*rightest_wrt_spine[1]/height,0)
     left_lower_point = (leftest_wrt_spine[0] -  wside*leftest_wrt_spine[1]/height,0)
     
+    # Compute the base of the parallelogram
     base = right_lower_point[0] - left_lower_point[0]
     width, min_w_vert, max_w_vert = compute_polygon_width(polygon,return_vertices=True)
     
-    if base >= width:
-        return (width,height,0), 0
-    
-    else:
+    # If the base of the parallelogram is smaller than the width of the polygon, return the parallelogram, as well as the left_lower_point
+    if base < width:
         return (base,height,wside), left_lower_point[0]
+    # Otherwise, return a bounding rectangle of the polygon instead
+    else:
+        return (width,height,0), 0
 
 def ordering_parallelograms_by_slope(parallelograms,return_map = False):
+    """
+    Orders a list of parallelograms by their slope (=wside/height).
+
+    Args:
+    - parallelograms (list of tuple): A list of parallelograms.
+    - return_map (bool, optional): If set to True, the function also returns the original indices of the sorted parallelograms. Defaults to False.
+
+    Returns:
+    - list or tuple: A list of parallelograms sorted by slope. If return_map is True, it returns the list and a map mapping the original indices to their new positions.
+    """
+    # Sort parallelograms based on the slope formed by height and wside
     parallelograms_sorted = sorted(parallelograms,key = lambda i:i[2]/i[1],reverse=False)
-    if return_map == True:
+    
+    if return_map:
+        # Return the sorted order and the corresponding map
         parallelograms_order = sorted(range(len(parallelograms)),key = lambda i:parallelograms[i][2]/parallelograms[i][1],reverse=False)
         parallelograms_map = sorted(range(len(parallelograms)),key = lambda i:parallelograms_order[i])
         return parallelograms_sorted, parallelograms_map
     return parallelograms_sorted
 
 def polygon_packing(polygons,c=3,rectangle_strip_packing_algorithm="ffdh",return_shelf_lines=False,return_rectangles_packing = False):
-    #Polygons need to be left- and bottom-adjusted.
-    
-    #Compute the bounding parallelogram of each polygon and compute w_max, the maximum width of any polygon.
+    """
+    Tightly packs a list of polygons into a strip of fixed width, using the algorithm from https://drops-beta.dagstuhl.de/entities/document/10.4230/LIPIcs.ESA.2023.76.
+
+    Args:
+    - polygons (list of list): A list of left- and bottom-adjusted polygons. Left- and bottom-adjusted means that the lowest x-coordinate vertex of the polygon has x-coordinate 0 and similar for the lowest y-coordinate vertex.
+    - c (int, optional): A constant determining the strip width. Defaults to 3 (which gives the best approximation guarantee).
+    - rectangle_strip_packing_algorithm (str, optional): The algorithm used for rectangle strip packing, either "ffdh" or "nfdh". Defaults to "ffdh".
+    - return_shelf_lines (bool, optional): If set to True, the function also returns lines that separate the shelves. Defaults to False.
+    - return_rectangles_packing (bool, optional): If set to True, the function returns data about how rectangles are packed. Defaults to False.
+
+    Returns:
+    - tuple: Depending on flags, the function returns the packed polygons, parallelograms, width and height of the packing, and optionally other information.
+    """    
+    # Compute the bounding parallelogram of each polygon and compute w_max, the maximum width of any polygon.
     parallelograms = []
     delta = []
     w_max = 0
     for polygon in polygons:
         parallelogram, delta_i = compute_bounding_parallelogram(polygon)
         parallelograms += [parallelogram]
-        delta += [delta_i]     #delta_i is the location of the left lower corner of the parallelogram with respect to (0,0) in the coordinate system of the polygon
+        delta += [delta_i] #delta_i is the location of the left lower corner of the parallelogram with respect to (0,0) in the coordinate system of the polygon
         w = compute_polygon_width(polygon)
         if w > w_max:
             w_max = w
     
+    # Define the width of the strip
     stripwidth = c*w_max    
     
-    #For each Polygon q=(base,height,wside), define a rectangle r=(base,height)
+    # For each Polygon q=(base,height,wside), define a rectangle r=(base,height)
     rectangles = [[parallelogram[0],parallelogram[1]] for parallelogram in parallelograms]
     
-    #Pack rectangles with FFDH or NFDH
+    # Pack rectangles with FFDH or NFDH algorithm
     if rectangle_strip_packing_algorithm == "ffdh":
         rectangles_shelves, heights_shelves, map_rectangles_shelves = ffdh(rectangles, stripwidth=stripwidth, return_map=True)
     elif rectangle_strip_packing_algorithm == "nfdh":
         rectangles_shelves, heights_shelves, map_rectangles_shelves = nfdh(rectangles, stripwidth=stripwidth, return_map=True)
     else:
-        print("Rectangle Strip Packing Algorithm \"",rectangle_strip_packing_algorithm,"\" is not supported.") 
+        print("Rectangle Shelf Packing Algorithm \"",rectangle_strip_packing_algorithm,"\" is not supported.") 
         return None
     
-    #Compute the shelves with the parallelograms and add a construct a map that, for any gives parallelogram, outpus its shelf number and the how manyeth polygon it is in that shelf.
+    # Compute the shelves with the parallelograms and create a mapping between parallelograms and their shelf number and order within that shelf.
     parallelograms_shelves = [[0]*len(shelf) for shelf in rectangles_shelves]
     for i in range(len(polygons)):
         parallelograms_shelves[map_rectangles_shelves[i][0]][map_rectangles_shelves[i][1]] = parallelograms[i]
     
+    # Order the parallelograms within each shelf by their slope
     parallelograms_shelves_ordered = [[] for x in range(len(rectangles_shelves))]
     parallelograms_shelves_ordering_map = [[] for x in range(len(rectangles_shelves))]
     for i in range(len(parallelograms_shelves)):
@@ -129,18 +194,20 @@ def polygon_packing(polygons,c=3,rectangle_strip_packing_algorithm="ffdh",return
     width = (c+2)*w_max
     height = sum(heights_shelves)
     
-    shelf_lines = [[(0,sum(heights_shelves[:i])),(width,sum(heights_shelves[:i]))] for i in range(len(heights_shelves)-1)]
-    side_lines = [[(w_max,0),(w_max,height)],[((c+1)*w_max,0),((c+1)*w_max,height)]]
+    output = (packed_polygons, packed_parallelograms, width, height)
     
-    lines = shelf_lines + side_lines
+    if return_shelf_lines:
+        # Define the shelf and side lines for visualization purposes
+        shelf_lines = [[(0,sum(heights_shelves[:i])),(width,sum(heights_shelves[:i]))] for i in range(len(heights_shelves)-1)]
+        side_lines = [[(w_max,0),(w_max,height)],[((c+1)*w_max,0),((c+1)*w_max,height)]]
         
-    if return_shelf_lines == True:
-        return packed_polygons, packed_parallelograms, width, height, lines
+        lines = shelf_lines + side_lines
+        output = output + (lines,)
     
-    if return_rectangles_packing == True:
-        return packed_polygons, packed_parallelograms, width, height, rectangles_shelves, heights_shelves
+    if return_rectangles_packing:
+        output = output + (rectangles_shelves, heights_shelves)
     
-    return packed_polygons, packed_parallelograms, width, height
+    return output
 
 def left_bottom_adjust_polygon(polygon):
     min_x = min(polygon,key=lambda i:i[0])[0]
